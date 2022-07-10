2d332ba2c6fc3edb6347ea2fddb547fac3fbdbe3
Christopher Lewis <tringlle@serversify.net>
2022-07-09 20:15:16 -0600

    fix it

--- src/main/java/veth/vetheon/survival/tasks/Healthboard.java
+++ src/main/java/veth/vetheon/survival/tasks/Healthboard.java
@@ -35,7 +35,6 @@ public class Healthboard extends BukkitRunnable {
         this.thirst = playerData.isInfoDisplayed(Info.THIRST);
         this.energy = playerData.isInfoDisplayed(Info.ENERGY);
         this.nutrients = playerData.isInfoDisplayed(Info.NUTRIENTS);
-        boolean test = playerData.isInfoDisplayed(Info.TEST);
 
         this.board.setTitle(Utils.getColoredString(plugin.getLang().healthboard_title));
 
