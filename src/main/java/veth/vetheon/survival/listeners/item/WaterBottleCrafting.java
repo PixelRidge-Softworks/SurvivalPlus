448fc5bb435e55e09fed330488846586af54e88b
VetheonGames <76892542+VetheonGames@users.noreply.github.com>
2022-08-21 19:33:47 -0600

    Removed unused import

--- src/main/java/veth/vetheon/survival/listeners/item/WaterBottleCrafting.java
+++ src/main/java/veth/vetheon/survival/listeners/item/WaterBottleCrafting.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
